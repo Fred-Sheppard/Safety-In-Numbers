895a89afb8227ff50c6e4d690bc94059820241f0
Fred Sheppard <frederickjssheppard@gmail.com>
2023-03-22 22:16:34 +0000

    Get absolute error all offsets

--- src/VarianceVsGroundTruth.java
+++ src/VarianceVsGroundTruth.java
@@ -16,8 +16,8 @@ public class VarianceVsGroundTruth {
     public static void main(String[] args) throws SQLException, IOException {
         Statement weatherModelsSQL = DriverManager.getConnection(
                 "jdbc:mariadb://localhost:3307/WeatherModels", "root", "1234").createStatement();
-        Statement variance = DriverManager.getConnection(
-                "jdbc:mariadb://localhost:3307/variance", "root", "1234").createStatement();
+        Statement offsetStatement = DriverManager.getConnection(
+                "jdbc:mariadb://localhost:3307/offsets", "root", "1234").createStatement();
         String[] tables = {"Accu_1h", "Aeris_1h", "ECMWF_1h", "Harmonie_1h", "OpenWeather_1h", "Visual_1h"};
 
         ArrayList<String> list = new ArrayList<>(Files.readAllLines(Paths.get("data/means.csv")));
@@ -30,45 +30,69 @@ public class VarianceVsGroundTruth {
         }
 
         for (String table : tables) {
-            variance.executeUpdate("drop table " + table);
-            variance.executeUpdate(String.format("""
+            // Reset the table
+            offsetStatement.executeUpdate("drop table " + table);
+            offsetStatement.executeUpdate(String.format("""
                     create table %s
                     (
-                        ID       bigint auto_increment
-                            primary key,
-                        Epoch    datetime null,
-                        Variance float    null,
-                        Absolute float    null
+                        ID       bigint primary key auto_increment,
+                        `Offset` int,
+                        Epoch    datetime,
+                        Variance float null,
+                        Absolute float null
                     );""", table));
-            StringBuilder builder = new StringBuilder();
-            builder.append(String.format("insert into %s (Epoch, Variance, Absolute)\nValues\n", table));
-            long multiplier = 1L;
-            int offset = 0;
-            if (table.equals("ECMWF_1h")) offset = 98;
-            if (table.equals("Visual_1h")) multiplier = 1000L;
-            String query = String.format("""
-                    SELECT * FROM %s
-                    WHERE Epoch > %d
-                    AND Epoch < %d
-                    AND `Offset` = %d;""", table, 1673827200L * multiplier, 1676678400L * multiplier, offset);
-            ResultSet resultSet = weatherModelsSQL.executeQuery(query);
-            while (resultSet.next()) {
-                long time = resultSet.getLong("Epoch");
-                double speed = resultSet.getDouble("WindSpeed");
-                Instant instant = switch (table) {
-                    case "Visual_1h" -> Instant.ofEpochMilli(time);
-                    default -> Instant.ofEpochSecond(time);
-                };
 
-                double diff = map.get(instant) - speed;
-                double abs = Math.abs(diff);
-                String tuple = String.format("('%s', %.2f, %.2f),\n", instant, diff, abs);
-                builder.append(tuple.replace("T", " ").replace("Z", ""));
+            double total = 0;
+
+            StringBuilder builder = new StringBuilder();
+            builder.append(String.format("insert into %s (Epoch, `Offset`, Variance, Absolute)\nValues\n", table));
+            long multiplier = switch (table) {
+                case "Visual_1h" -> 1000L;
+                default -> 1L;
+            };
+            int offset = switch (table) {
+                case "ECMWF_1h" -> 98;
+                default -> 0;
+            };
+            // Loop through offsets
+            while (true) {
+                System.out.print("\r" + offset);
+                String query = String.format("""
+                        SELECT * FROM %s
+                        WHERE Epoch > %d
+                        AND Epoch < %d
+                        AND `Offset` = %d;""", table, 1673827200L * multiplier, 1676678400L * multiplier, offset);
+                ResultSet resultSet;
+                // If offset goes out of bounds
+                try {
+                    resultSet = weatherModelsSQL.executeQuery(query);
+                } catch (SQLException e) {
+                    break;
+                }
+                // Check for offset too big
+                if (!resultSet.next()) break;
+                do {
+                    long time = resultSet.getLong("Epoch");
+                    double speed = resultSet.getDouble("WindSpeed");
+                    Instant instant = switch (table) {
+                        case "Visual_1h" -> Instant.ofEpochMilli(time);
+                        default -> Instant.ofEpochSecond(time);
+                    };
+
+                    double diff = map.get(instant) - speed;
+                    double abs = Math.abs(diff);
+                    String tuple = String.format("('%s', %d, %.2f, %.2f),%n", instant, offset, diff, abs);
+                    builder.append(tuple.replace("T", " ").replace("Z", ""));
+                }
+                while (resultSet.next());
+                offset++;
             }
-            builder.deleteCharAt(builder.length() - 1);
-            builder.deleteCharAt(builder.length() - 1);
+            builder.deleteCharAt(builder.length()-1);
+            builder.deleteCharAt(builder.length()-1);
+            builder.deleteCharAt(builder.length()-1);
             builder.append(";");
-            variance.executeUpdate(builder.toString());
+            offsetStatement.executeUpdate(builder.toString());
+//            System.out.println(builder);
         }
     }
 }
